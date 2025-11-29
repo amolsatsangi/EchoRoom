84c2957340cf57fe4e99c93fa285521e2ef906ea
AMOL SATSANGI <15amolsat@gmail.com>
2025-11-29 23:38:59 +0530

    Added message.hpp file containing all info regrarding messgaes

---
+++ message.hpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<memory>
+#ifndef CHATROOM_HPP
+#define CHATROOM_HPP
+#include <cstring>
+class Message{
+    public:
+        Message() : bodyLength_{0}{
+        }
+        Message(std::string message){
+            bodyLength_ = message.size()>maxBytes?maxBytes:message.size();
+            encodeHeader();
+            memcpy(data+header,message.c_str(),bodyLength_);
+        }
+        void encodeHeader(){
+            char new_header[header+1] = "";
+            sprintf(new_header,"%4d", int(bodyLength_));
+            memcpy(data,new_header,header);
+        }   
+        std::string getData(){
+            int length = header+bodyLength_;
+            std::string result(data,length);
+            return result;
+        }
+        std::string getBody(){
+            std::string fullData = getData();
+            std::string message = fullData.substr(header,bodyLength_);
+            return message;
+        }
+        bool decodeHeader(){
+            char newHeader[header+1] = "";
+            strncpy(newHeader,data,header);
+            newHeader[header] = '\0';
+            int newHeaderValue = atoi(newHeader);
+            if(newHeaderValue>maxBytes)
+                return false;
+            bodyLength_ = newHeaderValue;
+            return true;
+
+        }
+    private:
+        enum {header = 4};
+        enum {maxBytes = 512};
+        char data[header+maxBytes];
+        size_t bodyLength_;
+        
+    
+};
+
+#endif
